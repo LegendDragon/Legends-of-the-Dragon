6f6d8bb014ea8cc4ddecaee7b058c09e13838b50
stalry221 <bartas@hotmail.no>
2014-07-25 03:24:37 +0200

    Update Game.java
    added Game constructor and main function to the game file

--- Main/Game.java
+++ Main/Game.java
@@ -2,4 +2,16 @@ package Main;
 
 public class Game {
 
+  private int width = 320;
+  private int height = 240;
+  private int scale = 2;
+  
+  public Game() {
+    
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+
 }
